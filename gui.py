9a11c160b170d165d1266c18f69cf413b2ed7a83
Velthil <79408617+Velthil@users.noreply.github.com>
2022-10-28 19:51:17 +0200

    v0.0.0.0.1

--- gui.py
+++ gui.py
@@ -2,9 +2,16 @@ import tkinter as tk
 
 
 class Gui(tk.Tk):
+
     def __init__(self):
         super().__init__()
         self.title('DB fill')
         self.geometry('800x600')
 
+        self.label1 = tk.Label(self, text='Wybierz tabele:', font=('Arial', 18))
+        self.label1.grid(row=0, column=0, columnspan=2)
+
+        var = tk.IntVar()
+        self.chk = tk.Checkbutton(self, text='krwiodawcy', font=('Arial', 16))
+        self.chk.grid(row=1, column=1)
 
